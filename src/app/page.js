ec62ef8a97d45e3f3af4b38356b8984dc4c2782c
RomeroPadillaFranciscoOmar <francisco_1321124145@uptecamac.edu.mx>
2024-07-30 13:16:59 -0600

    segundo cambio imagenes

--- src/app/page.js
+++ src/app/page.js
@@ -25,42 +25,47 @@ export default function Portada() {
         </Link>
       </nav>
 
-      <div className="flex flex-col items-center justify-center flex-1 relative p-4">
-        <div className="absolute top-4 left-4">
-          <Image
-            src="/imagenes-logos/logo1.jpg"
-            alt="Logo 1"
-            width={100}
-            height={100}
-          />
-        </div>
-        <div className="absolute top-4 right-4">
-          <Image
-            src="/imagenes-logos/logo2.png"
-            alt="Logo 2"
-            width={100}
-            height={100}
-          />
+      <div className="relative p-4">
+        <div className="flex flex-col items-center justify-center absolute inset-x-0 top-4 px-4">
+          <div className="absolute top-4 left-4">
+            <Image
+              src="/imagenes-logos/logo1.jpg"
+              alt="Logo 1"
+              width={120}
+              height={120}
+              className="object-contain"
+            />
+          </div>
+          <div className="absolute top-4 right-4">
+            <Image
+              src="/imagenes-logos/logo2.png"
+              alt="Logo 2"
+              width={120}
+              height={120}
+              className="object-contain"
+            />
+          </div>
         </div>
 
-        <div className="text-center space-y-6">
-          <h1 className="text-4xl font-bold">CARRERA:</h1>
-          <h1 className="text-4xl font-bold">INGENIERÍA EN SOFTWARE</h1>
-          <h2 className="text-2xl font-bold">MATERIA:</h2>
-          <h2 className=" text-2xl">SEGURIDAD DE LA INFORMACION</h2>
-          <h2 className="text-2xl font-bold ">GRUPO:</h2>
-          <h2 className="text-2xl">3924-IS</h2>
-          <h2 className="text-2xl font-bold">EVIDENCIA:</h2>
-          <h2 className="text-2xl">SITIO WEB</h2>
-          <h2 className="text-2xl font-bold">PROFESOR:</h2>
-          <h2 className="text-2xl">NESTOR APOLO LOPEZ GONZALEZ</h2>
-          <h2 className="text-2xl font-bold">ALUMNO:</h2>
-          <h2 className="text-2xl">ROMERO PADILLA FRANCISCO OMAR</h2>
-          <h2 className="text-2xl font-bold">MATRÍCULA:</h2>
-          <h2 className="text-2xl">1321124145</h2>
+        <div className="pt-24">
+          <div className="text-center">
+            <h1 className="text-2xl font-bold">CARRERA:</h1>
+            <h1 className="text-2xl font-bold">INGENIERÍA EN SOFTWARE</h1>
+            <h2 className="text-2xl font-bold">MATERIA:</h2>
+            <h2 className=" text-2xl">SEGURIDAD DE LA INFORMACION</h2>
+            <h2 className="text-2xl font-bold ">GRUPO:</h2>
+            <h2 className="text-2xl">3924-IS</h2>
+            <h2 className="text-2xl font-bold">EVIDENCIA:</h2>
+            <h2 className="text-2xl">SITIO WEB</h2>
+            <h2 className="text-2xl font-bold">PROFESOR:</h2>
+            <h2 className="text-2xl">NESTOR APOLO LOPEZ GONZALEZ</h2>
+            <h2 className="text-2xl font-bold">ALUMNO:</h2>
+            <h2 className="text-2xl">ROMERO PADILLA FRANCISCO OMAR</h2>
+            <h2 className="text-2xl font-bold">MATRÍCULA:</h2>
+            <h2 className="text-2xl">1321124145</h2>
+          </div>
         </div>
       </div>
-
       <footer className="bg-gray-800 text-white text-center py-4">
         <p className="text-sm">
           © {new Date().getFullYear()} Todos los derechos reservados.
